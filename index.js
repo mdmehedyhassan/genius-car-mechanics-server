a5c35efdc149d4f06c60a7f4c0422b2e4f1e772d
unknown <mdmehedyh55@gamil.com>
2022-02-06 14:51:57 +0600

    port added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 // middle
